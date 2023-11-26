6052464010661328df625848e43237504f25fdcd
Itallo <itallo.oliver21@gmail.com>
2023-11-25 22:36:25 -0300

    modificações no servidor no login

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 import socket
-
+lista_usuarios = {
+    
+    "fernando": "123"
+}
 def start_server(port):
+    
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("localhost", port))
+    server.bind(("0.0.0.0", port))
     server.listen(1)#socket do servidor especifica o número máximo de conexões pendentes que podem ser enfileiradas enquanto o servidor está lidando com uma conexão existente.
     
     print("Bem Vindo ao Chat!")
@@ -12,7 +16,29 @@ def start_server(port):
     print(f"Conexão estabelecida com {end_client}")
 
     while True:
-        msg_client = connection.recv(1024).decode("utf-8")
+        msg_client = connection.recv(1024).decode("utf-8").split(" ")
+        
+        if msg_client[0] == "login":                                                                          
+            if msg_client[1] in lista_usuarios:
+                if lista_usuarios[msg_client[1]] ==  msg_client[2]:
+                    connection.send("Login efetuado!".encode('utf-8'))
+                    continue
+                else:
+                    connection.send("Usuário ou senha incorreto.".encode('utf-8'))
+                    continue
+            else:
+                connection.send("Usuário ou senha incorreto.".encode('utf-8'))
+                continue
+        if msg_client[0] == "register":                                                                          
+            if msg_client[1] in lista_usuarios:
+                connection.send("erro".encode('utf-8'))
+                continue
+            lista_usuarios[msg_client[1]] = msg_client[2]
+            connection.send("ok".encode('utf-8'))
+            print(lista_usuarios)
+            continue
+               
+                    
         if not msg_client:
             break
         print(f"Cliente: {msg_client}")
