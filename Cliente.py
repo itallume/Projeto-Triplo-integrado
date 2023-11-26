8814fb5d2ca2b8567bf281b558f742ca9dd772db
FernandoJMartins <julio8martins@gmail.com>
2023-11-25 22:38:38 -0300

    Alteracao dos Metodos do cliente

--- Cliente.py
+++ Cliente.py
@@ -2,19 +2,38 @@ import socket
 
 def start_client(port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(("localhost", port))
+    client.connect(("25.3.52.178", port))
 
     print("1 - login\n2 - signup")
     
-
     loginChoice = int(input("Escolha: "))
-    if loginChoice == 1:
-        login()
-    if loginChoice == 2:
-        sign()
-    else:
-        print("Não foi possivel Logar, tente novamente")
-        return #remover esse break e tratar depois
+    
+    
+    while True:
+        if loginChoice == 1:
+            log = login()
+            client.send(log.encode("utf-8"))
+            validate_login = client.recv(1024).decode("utf-8")
+            print(f"Servidor: {validate_login}")
+
+            if validate_login != "Usuário ou senha incorreto.":
+                break
+
+
+        elif loginChoice == 2:
+            register = signup()
+            client.send(register.encode("utf-8"))
+            validate_register = client.recv(1024).decode("utf-8")
+            print(f"Servidor: {validate_register}")
+
+            if validate_register != "erro":
+                break
+
+
+
+        else:
+            print("Não foi possivel Logar, tente novamente")
+            return #remover esse break e tratar depois
 
 
     while True:
@@ -32,10 +51,16 @@ def start_client(port):
 
 
 def login():
-    pass
+    user = input("usuario: ").lower()
+    password = input("senha: ").lower()
+    response = f"login {user} {password}"
+    return response
 
-def sign():
-    pass
+def signup():
+    user = input("usuario: ").lower()
+    password = input("senha: ").lower()
+    response = f"register {user} {password}"
+    return response
     
 def menu():
     return('''
